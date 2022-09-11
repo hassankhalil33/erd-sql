556ee0ff0aa203e9b830651a802b09384eb70cad
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-11 16:13:01 +0300

    Fixed Best-Picture Query

--- first-db/best-picture-winner-sq.sql
+++ first-db/best-picture-winner-sq.sql
@@ -1,3 +1,3 @@
 SELECT movies.name, movies.earnings_rank FROM movies, oscars
-WHERE movies.id = oscars.movie_id 
+WHERE movies.id = oscars.movie_id AND oscars.type = "Best-Picture"
 ORDER BY movies.earnings_rank LIMIT 1;
