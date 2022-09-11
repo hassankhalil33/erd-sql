74f7c55e39e5d5d0772083f34b4b0bbefb788cdd
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-11 15:37:46 +0300

    Added Not Enrolled Query

--- second-db/uni-search-query.sql
+++ second-db/uni-search-query.sql
@@ -8,9 +8,17 @@ WHERE capacity > 100;
 SELECT name FROM courses
 WHERE start_time = (SELECT MIN(start_time) FROM courses);
 
--- Find Courses of BIF Major || ASSUMES NO STUDENT CAN TAKE COURSES OUTSIDE MAJOR ||
+-- Find courses of BIF major || ASSUMES NO STUDENT CAN TAKE COURSES OUTSIDE MAJOR ||
 
 SELECT courses.name FROM courses, departments, enrolls, students, majors
 WHERE departments.name = "BIF" AND courses.id_crn = enrolls.courses_id_crn
 AND enrolls.students_id = students.id AND students.id = majors.students_id
 AND majors.departments_id = departments.id;
+
+-- Students not enrolled
+
+SELECT students.name FROM students, courses, enrolls
+WHERE students.name NOT IN(SELECT students.name FROM students, enrolls, courses
+WHERE students.id = enrolls.students_id AND courses.id_crn = enrolls.courses_id_crn
+GROUP BY students.name)
+GROUP BY students.name;
